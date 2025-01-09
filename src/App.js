af9eed24cab82e16c11c877623bf697d730edacb
Jocgonham <jocgonham@gmail.com>
2025-01-09 18:25:27 -0300

    dom dinamico
    DOM DINAMICO

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Welcome from './componets/Welcome';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Contact from './pages/contact';
+import AboutHer from './pages/aboutmañosa';
+import Profile from './pages/Profile';
 function App() {
   return (
     <div className="App">
@@ -17,10 +19,18 @@ function App() {
               <li>
                 <Link to="/contact">Contacto</Link>
               </li>
+              <li>
+                <Link to="/aboutmañosa">Mañosa</Link>
+              </li>
+              <li>
+                <Link to="/Profile">Profile</Link>
+              </li>
             </ul>
           </div>
           <Routes>
             <Route path="/contact" element={<Contact/>} />
+            <Route path="/aboutmañosa" element={<AboutHer/>} />
+            <Route path="/profile/:name" element={<Profile/>} />
           </Routes>
         </Router>
         <Welcome message ="Hola Welcome props" name="Tm0"/>
---
+++ src/pages/aboutmañosa.js
@@ -0,0 +1,7 @@
+export default function AboutHer() {
+    return(
+        <div>
+            <h3>Our little house</h3>
+        </div>
+    );
+}
